9733a1f7a6c9682d19898ed11c363de127652414
Yurico Hernandez <serenumia@gmail.com>
2026-09-23 22:55:42 -0400

    9/23 changed api link & added lists

--- v2/main.py
+++ v2/main.py
@@ -14,7 +14,7 @@ def event():
 
     api_key = os.environ.get("TBA_API_KEY")
 
-    url = f"https://www.thebluealliance.com/api/v3/event/{event_code}/matches"
+    url = f"https://www.thebluealliance.com/api/v3/team/frc4472/event/{event_code}/matches/simple"
 
     headers = {
         "X-TBA-Auth-Key": api_key
@@ -24,10 +24,23 @@ def event():
 
     matches = response.json()
 
-    return render_template(
-        "event.html",
-        matches=matches
-    )
+    qm_matches = []
+    for match in matches:
+        if match["comp_level"] == "qm":
+            qm_matches.append(match)
+
+    sf_matches = []
+    for match in matches:
+        if match["comp_level"] == "sf":
+            sf_matches.append(match)
+
+    f_matches = []
+    for match in matches:
+        if match["comp_level"] == "f":
+            f_matches.append(match)
+
+    return render_template('event.html', quali_matches=qm_matches, semifinal_matches=sf_matches, final_matches=f_matches)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
